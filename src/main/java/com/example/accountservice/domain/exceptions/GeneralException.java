df8c356e68c94a9cbeaf7482c86ee6539249159f
MedOussBougu <mohamedoussama.bougueddach@gmail.com>
2022-12-25 09:29:58 +0000

    Fix version issues

---
+++ src/main/java/com/example/accountservice/domain/exceptions/GeneralException.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.example.accountservice.domain.exceptions;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author mBougueddach
+ */
+public class GeneralException extends RuntimeException{
+    /**
+     * code : failed Method name
+     * message : message to show
+     */
+    private final String code;
+    private final String message;
+
+
+    public GeneralException(String code,String message) {
+        super(message);
+        this.code = code;
+        this.message = message;
+    }
+
+    public GeneralException(String code,String message, Throwable cause) {
+        super(message, cause);
+        this.code = code;
+        this.message = message;
+    }
+    public GeneralException(String code,Exception exception) {
+        super(exception.getMessage(), exception.getCause());
+        this.code = code;
+        this.message = exception.getMessage();
+    }
+    public GeneralException(Exception exception) {
+        super(exception.getMessage(), exception.getCause());
+        this.code = exception.getClass().getSimpleName();
+        this.message = exception.getMessage();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GeneralException that = (GeneralException) o;
+        return code.equals(that.code) && message.equals(that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, message);
+    }
+}
---
+++ src/main/java/com/example/accountservice/security/MyUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.example.accountservice.security;
+
+import java.util.Collections;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.accountservice.domain.models.dtos.UserAccountDTO;
+import com.example.accountservice.domain.services.UserAccountService;
+
+import lombok.RequiredArgsConstructor;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mBougueddach
+ */
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+    private final UserAccountService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return userService.loadUserByUsername(username);
+    }
+}
